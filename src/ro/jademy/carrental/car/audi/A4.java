b79eebffa71b690eaeb4fe28b908d8078efced90
Ana <anamagav@yahoo.com>
2018-10-04 20:53:27 +0300

    Implementation of the new methods

--- src/ro/jademy/carrental/car/audi/A4.java
+++ src/ro/jademy/carrental/car/audi/A4.java
@@ -1,4 +1,4 @@
-package ro.jademy.carrental.car.Audi;
+package ro.jademy.carrental.car.audi;
 
 import ro.jademy.carrental.car.Engine;
 
@@ -6,9 +6,9 @@ import java.math.BigDecimal;
 
 public class A4 extends Audi {
     public A4(String make, String model, String carType, Engine engine, Integer doorNumber, String fuelType,
-              String color, String transmissionType, Integer year,BigDecimal basePrice,  String availability){
-        super("Audi", "A4", carType, engine, doorNumber, fuelType,
-                color, transmissionType,year, basePrice,  availability) ;
+              String color, String transmissionType, Integer year, BigDecimal basePrice) {
+        super("audi", "A4", carType, engine, doorNumber, fuelType,
+                color, transmissionType, year, basePrice);
 
     }
 }
--- src/ro/jademy/carrental/car/audi/Audi.java
+++ src/ro/jademy/carrental/car/audi/Audi.java
@@ -1,4 +1,4 @@
-package ro.jademy.carrental.car.Audi;
+package ro.jademy.carrental.car.audi;
 
 import ro.jademy.carrental.car.Car;
 import ro.jademy.carrental.car.Engine;
@@ -7,8 +7,8 @@ import java.math.BigDecimal;
 
 public abstract class Audi extends Car {
     public Audi(String make, String model, String carType, Engine engine, Integer doorNumber, String fuelType,
-                String color, String transmissionType, Integer year, BigDecimal basePrice,  String availability) {
-        super("Audi", model, carType, engine, doorNumber, fuelType,
+                String color, String transmissionType, Integer year, BigDecimal basePrice) {
+        super("audi", model, carType, engine, doorNumber, fuelType,
                 color, transmissionType, year, basePrice);
     }
 }
